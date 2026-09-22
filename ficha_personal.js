9600efdbb51530d969c4a53c3c003a3222d6ba28
Mia Ares <glitchares@gmail.com>
2026-09-22 10:54:54 -0300

    feat: input disponible desde la biblioteca npm y modificación de los datos de la ficha personal

--- ficha_personal.js
+++ ficha_personal.js
@@ -1,21 +1,29 @@
-/*Crea un programa que Guarde en variables
-Nombre: string
-Edad: number
-Cantidad de horas que destina a programar a la
-semana: number
-Le gusta programar: booleano
-Es mayor de edad: decide cual deberia ir aca
-Luego debe IMPRIMIR TODO POR CONSOLA
-*/
+import promptSync from "prompt-sync"
 
-let nombres = x;
-let edad = x;
-let horasSemanalesProgramacion = x;
-let leGustaProgramar = x;
-let esMayorDeEdad = x;
+const prompt = promptSync() 
 
-console.log("Ficha Personal");
-console.log("Nombre:");
-console.log("Cantidad de horas que destina a programar a la semana:", 5) 
-console.log("Le gusta programar:", true);
-console.log("Es mayor de edad:", true) 
+let nombre = prompt("Insertá tu nombre: ");
+let edad = prompt("Insertá tu edad: ");
+let horasSemanalesProgramacion = prompt("¿Cuántas horas programás a la semana?: ");
+let leGustaProgramar = prompt("¿Te gusta programar?: ");
+
+console.log("--FICHA PERSONAL--");
+console.log("Nombre:", nombre );
+console.log("Cantidad de horas de programación semanales:", horasSemanalesProgramacion); 
+
+
+if (leGustaProgramar == "sí" )
+    console.log("Le gusta programar")
+else
+    console.log("No le gusta programar") 
+
+if (edad >= 18) 
+    console.log("Es mayor de edad") 
+else
+    console.log("Es menor de edad")
+
+// es mayor de edad espera un booleano, quiero hacerle un condicional por string si o por no.
+
+// Existen methods como .lower()?
+
+// estructura condicionales, llaves sí o no? 
