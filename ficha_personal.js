e436dd5a3de7015d01feb606b9f7098993334cc9
Mia Ares <glitchares@gmail.com>
2026-09-22 09:43:10 -0300

    feat: código inicial de la ficha personal

--- ficha_personal.js
+++ ficha_personal.js
@@ -7,3 +7,15 @@ Le gusta programar: booleano
 Es mayor de edad: decide cual deberia ir aca
 Luego debe IMPRIMIR TODO POR CONSOLA
 */
+
+let nombres = x;
+let edad = x;
+let horasSemanalesProgramacion = x;
+let leGustaProgramar = x;
+let esMayorDeEdad = x;
+
+console.log("Ficha Personal");
+console.log("Nombre:");
+console.log("Cantidad de horas que destina a programar a la semana:", 5) 
+console.log("Le gusta programar:", true);
+console.log("Es mayor de edad:", true) 
